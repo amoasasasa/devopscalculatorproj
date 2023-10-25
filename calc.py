19be3b7c929b38eb089478fb588f66617dbd5c7b
amoasasasa <chavanac20001@gmail.com>
2023-10-25 20:14:39 +0530

    done error handling using try,except

--- calc.py
+++ calc.py
@@ -1,27 +1,32 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/',methods=['GET','POST'])
 
+@app.route('/',methods=['GET','POST'])
 
 def index():
     result = None
     if request.method == 'POST':
-        num1 = float(request.form['num1'])
-        num2 = float(request.form['num2'])
+        try :
+            num1 = float(request.form['num1']) 
+            num2 = float(request.form['num2'])
+        except ValueError:
+            result = "enter integer"
+            return render_template('calculator.html',result=result)
         operator = request.form['operator']
-        
         if operator == 'add':
-            result = num1 + num2
+            result = float(num1 + num2)
         elif operator == 'subtract':
-            result = num1 - num2
+            result = float(num1 - num2)
         elif operator == 'multiply':
-            result = num1 * num2
+            result = float(num1 * num2)
         elif operator == 'divide':
-            result = num1 / num2
-    
+            result = float(num1 / num2)
+        else :
+            result = "invaid operator"
+            return render_template('calculator.html',result=result)
     return render_template('calculator.html',result=result)
 
 if __name__ == '__main__':
     #add host ip address on which app is going to run
-    app.run(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0',debug=True)  
